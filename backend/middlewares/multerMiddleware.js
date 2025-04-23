c1f8c450dab828baea354be49247eda29dcd2109
Roroshi7 <roshanshiroorkar@gmail.com>
2025-04-24 00:21:38 +0530

    Added controller

--- backend/middlewares/multerMiddleware.js
+++
@@ -1,6 +0,0 @@
-import multer from "multer";
-
-const storage = multer.memoryStorage(); // Store in memory for direct Cloudinary upload
-const upload = multer({ storage: storage });
-
-export default upload;
